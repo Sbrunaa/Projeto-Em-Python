e9b8f70640a60bb243821c1a8ec85afaccef6f20
Bruna Santos <83190947+Sbrunaa@users.noreply.github.com>
2026-05-23 23:27:40 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,28 @@
-user = input()
-password = input()
+option = ""
+users = {}
 
+while option !="0":
 
+    print("------------------------Menu---------------------------")
+    print("    Digite 1 - Cadastro.")
+    print("    Digite 2 - Login.")
+    print("    Digite 0 - Sair.")
+    print()
+    option =  input("Escolha a opção:")  
 
-if user == "bruna" and password == 123:
-    print("User valido")
-else:
-    print("User invalido, por favor insira os dados corretos!")
+    if option == "1":
+        new_user = input("User:")
+        new_password = input("Password:")
+
+        users[new_user] = new_password
+    elif option == "2":
+
+        user =  input("User:")
+        password = input("Password:")
+        if user in users and users[user] == password:
+            print("Usuario válido!")
+        else:
+            print("Usuario invalido!")
+
+    elif option == "0":
+        break
